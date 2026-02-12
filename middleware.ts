f9a93b8937d30ca13af0630db68a5d61d361fe98
Sean Tarnowski <tarnowski.sean@gmail.com>
2026-02-12 07:41:43 -0800

    chore: force deploy v5 to ensure user is on latest code

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 
 export default NextAuth(authConfig).auth
+// Force Deployment v5 - Ensure debug-page is accessible
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico|debug-page).*)"],
